511894844818cd2468cbf042990bc6a680fd6312
Andres Rodriguez <andres.rodriguez0215@gmail.com>
2025-04-21 17:16:28 -0500

    Feat: create app.css with base styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="t__base t__heading--l">Design System</h1>
+
+<div class="ds__container">
+  <span class="t__base t__heading--l"> Heading (L) - Outfit Light - 32px</span>
+  <span class="t__base t__heading--m"> Heading (M) - Outfit Light - 24px</span>
+  <span class="t__base t__heading--s"> Heading (S) - Outfit Medium - 24px</span>
+  <span class="t__base t__heading--xs"> Heading (XS) - Outfit Medium - 18px</span>
+
+  <span class="t__base t__body--m"> Body (M) - Outfit Light - 15px</span>
+  <span class="t__base t__body--s"> Body (S) - Outfit Light - 13px</span>
+</div>
+
+<style>
+  .ds__container {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+</style>
